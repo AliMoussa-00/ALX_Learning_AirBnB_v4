f193346811c5085ad0e57d72ab3b4e394cc22793
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-02-08 22:42:00 +0100

    task .3 - CORS errors

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,32 @@
+// untill the window and DOM is ready
+$(function () {
+
+    let amenities = {}
+
+    /* let's go through all the checkbox to see if they are already checked and add them to the list*/
+    $('.popover li input').each(function () {
+        if (this.checked) {
+            amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+        }
+    })
+    if (amenities) {
+        $(".filters .amenities h4").text(Object.values(amenities).join(', '));
+    }
+
+    /* listen to changes of the checkbox */
+    $('.popover li input').change(function () {
+
+        if (this.checked) {
+            amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+        }
+        else {
+            delete amenities[$(this).attr('data-id')]
+        }
+
+        $(".filters .amenities h4").text(Object.values(amenities).join(', '));
+    })
+
+    $.get("http://0.0.0.0:5001/api/v1/status/", function (response) {
+        console.log(response);
+    })
+});
