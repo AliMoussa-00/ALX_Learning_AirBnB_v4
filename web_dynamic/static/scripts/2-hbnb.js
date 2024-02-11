5735336d9ceef848bf6c0550b4bd94a5c3e83818
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-02-11 11:04:50 +0100

    change style of 2-hbnb 3-hbnb

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,42 +1,34 @@
 // untill the window and DOM is ready
 $(function () {
+  const amenities = {};
 
-    let amenities = {}
+  /* let's go through all the checkbox to see if they are already checked and add them to the list */
+  $('.popover li input').each(function () {
+    if (this.checked) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    }
+  });
+  if (amenities) {
+    $('.filters .amenities h4').text(Object.values(amenities).join(', '));
+  }
 
-    /* let's go through all the checkbox to see if they are already checked and add them to the list*/
-    $('.popover li input').each(function () {
-        if (this.checked) {
-            amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-        }
-    })
-    if (amenities) {
-        $(".filters .amenities h4").text(Object.values(amenities).join(', '));
+  /* listen to changes of the checkbox */
+  $('.popover li input').change(function () {
+    if (this.checked) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
     }
 
-    /* listen to changes of the checkbox */
-    $('.popover li input').change(function () {
+    $('.filters .amenities h4').text(Object.values(amenities).join(', '));
+  });
 
-        if (this.checked) {
-            amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-        }
-        else {
-            delete amenities[$(this).attr('data-id')]
-        }
-
-        $(".filters .amenities h4").text(Object.values(amenities).join(', '));
-    })
-
-    /* get the status of the request */
-    // $.get("http://0.0.0.0:5001/api/v1/status/", function (response) {
-    //     console.log(response);
-    // })
-    $.get("http://localhost:5001/api/v1/status/", function (response) {
-        console.log(response.status)
-        if (response.status === "OK") {
-            $("#api_status").addClass("available")
-        }
-        else {
-            $("#api_status").removeClass("available")
-        }
-    })
+  $.get('http://localhost:5001/api/v1/status/', function (response) {
+    console.log(response.status);
+    if (response.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
 });
